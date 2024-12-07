c6e5d6abf099c0f02d249ca05f8aab950ac3e8ec
ricky-kiva <ricky.kiva@gmail.com>
2024-12-07 23:47:28 +0700

    feat(domain): add `ThreadCommentRepository` entity & its test

---
+++ src/Domains/thread_comments/ThreadCommentRepository.js
@@ -0,0 +1,8 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+
+class ThreadCommentRepository {
+  async addThreadComment(threadComment) {
+    throw new Error('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
---
+++ src/Domains/thread_comments/_test/ThreadCommentRepository.test.js
@@ -0,0 +1,10 @@
+const ThreadCommentRepository = require('../ThreadCommentRepository');
+
+describe('ThreadCommentRepository interface', () => {
+  it('throws error when invoking abstract behavior', async () => {
+    const threadCommentRepository = new ThreadCommentRepository();
+
+    await expect(threadCommentRepository.addThreadComment({}))
+      .rejects.toThrow('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
