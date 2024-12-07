4ede2a9ef19252f76d74e75cdd8ca637313095b7
ricky-kiva <ricky.kiva@gmail.com>
2024-12-07 19:51:09 +0700

    feat(migration): add migration for `Threads` table

---
+++ migrations/1733571916449_add-foreign-key-to-owner-column-of-threads-table.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.addConstraint(
+    'threads',
+    'fk_threads.owner_users.id',
+    'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('threads', 'fk_threads.owner_users.id');
+};
