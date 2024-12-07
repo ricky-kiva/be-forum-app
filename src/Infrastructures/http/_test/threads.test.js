e9a70edc588f8551047dad4af5caf62e4d08b6c6
ricky-kiva <ricky.kiva@gmail.com>
2024-12-07 23:22:35 +0700

    test(http): add test template to `threads` endpoint test

--- src/Infrastructures/http/_test/threads.test.js
+++ src/Infrastructures/http/_test/threads.test.js
@@ -1,1 +1,5 @@
-// TODO: add test for '/threads' endpoint
+describe('when POST /threads', () => {
+  it('should response 201 and persisted thread', async () => {
+    // TODO: add test for '/threads' endpoint
+  });
+});
