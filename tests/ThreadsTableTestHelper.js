c8205714bdaeba926ef257be0006ddad03f8b87e
ricky-kiva <ricky.kiva@gmail.com>
2024-12-08 11:26:40 +0700

    feat(db-helper): add `addThread` func.

--- tests/ThreadsTableTestHelper.js
+++ tests/ThreadsTableTestHelper.js
@@ -1,8 +1,21 @@
 const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const ThreadsTableTestHelper = {
+  async addThread({
+    id = 'thread-123',
+    title = 'Thread Title',
+    body = 'Thread body',
+    owner = 'user-123',
+  }) {
+    const q = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4)',
+      values: [id, title, body, owner],
+    };
+
+    await pool.query(q);
+  },
   async cleanTable() {
-    await pool.query('TRUNCATE TABLE threads');
+    await pool.query('TRUNCATE TABLE threads CASCADE');
   },
 };
 
