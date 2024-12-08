42f40af5faddc85469428468bc5c236f82e1d772
ricky-kiva <ricky.kiva@gmail.com>
2024-12-08 13:58:51 +0700

    test(infra-http): add `threadComments` test TODOs

---
+++ src/Infrastructures/http/_test/threadComments.test.js
@@ -0,0 +1,5 @@
+describe('when POST /threads/{threadId}/comments', () => {
+  it('should response 201 and persisted thread comment', async () => {
+    // TODO: add test for '/threads/{threadId}/comments' endpoint
+  });
+});
