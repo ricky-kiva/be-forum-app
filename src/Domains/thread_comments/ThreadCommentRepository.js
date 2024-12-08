44d27025dda2cd4f082a9d6290fdb5a0a8a089c6
ricky-kiva <ricky.kiva@gmail.com>
2024-12-08 15:14:19 +0700

    feat(domain): add delete Thread Comment func. to interface

--- src/Domains/thread_comments/ThreadCommentRepository.js
+++ src/Domains/thread_comments/ThreadCommentRepository.js
@@ -5,6 +5,10 @@ class ThreadCommentRepository {
   async addThreadComment(threadComment) {
     throw new Error('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async deleteThreadComment(id) {
+    throw new Error('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ThreadCommentRepository;
--- src/Domains/thread_comments/_test/ThreadCommentRepository.test.js
+++ src/Domains/thread_comments/_test/ThreadCommentRepository.test.js
@@ -6,5 +6,8 @@ describe('ThreadCommentRepository interface', () => {
 
     await expect(threadCommentRepository.addThreadComment({}))
       .rejects.toThrow('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+
+    await expect(threadCommentRepository.deleteThreadComment(''))
+      .rejects.toThrow('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
