b423d0575331f80704461bedd7c50a7dd9222e71
ricky-kiva <ricky.kiva@gmail.com>
2024-12-13 02:15:33 +0700

    test(use-case): adj. `GetThreadByIdResponseUseCase` test according to exec. timeline

--- src/Applications/use_case/_test/GetThreadByIdResponseUseCase.test.js
+++ src/Applications/use_case/_test/GetThreadByIdResponseUseCase.test.js
@@ -7,65 +7,66 @@ const UserRepository = require('../../../Domains/users/UserRepository');
 
 describe('GetThreadByIdResponseUseCase', () => {
   it('should oscestrate get Thread by id action correctly', async () => {
-    const threadId = 'thread-123';
-    const username = 'user123';
-    const date = 'fixed-date';
-    const credentialId = 'user-123';
-    const isDelete = false;
-
-    const mockThreadEntity = new ThreadEntity({
-      id: threadId,
-      title: 'Thread title',
-      body: 'Thread body',
-      owner: credentialId,
-      date,
-    });
-
-    const mockThreadCommentEntities = [
-      new ThreadCommentEntity({
-        id: 'comment-123',
-        content: 'Thread body 123',
-        owner: credentialId,
-        thread: threadId,
-        isDelete,
-        date,
-      }),
-      new ThreadCommentEntity({
-        id: 'comment-124',
-        content: 'Thread body 124',
-        owner: 'user-124',
-        thread: threadId,
-        isDelete,
-        date,
-      }),
-    ];
-
-    const expectedComments = [
-      {
-        id: 'comment-123',
-        content: 'Thread body 123',
-        thread: threadId,
-        date,
-        username,
-      },
-      {
-        id: 'comment-124',
-        content: 'Thread body 124',
-        thread: threadId,
-        date,
-        username,
-      },
-    ];
-
     const mockThreadRepository = new ThreadRepository();
     const mockUserRepository = new UserRepository();
     const mockThreadCommentRepository = new ThreadCommentRepository();
 
+    const idNumber1 = (123).toString();
+    const idNumber2 = (idNumber1 + 1).toString();
+
+    const threadId = `thread-${idNumber1}`;
+    const title = 'Thread title';
+    const body = 'Thread body';
+    const credentialId = `user-${idNumber1}`;
+    const date = 'fixed-date';
+
+    const mockThreadEntity = new ThreadEntity({
+      id: threadId,
+      title,
+      body,
+      owner: credentialId,
+      date,
+    });
+
     mockThreadRepository.getThreadById = jest.fn()
       .mockImplementation(() => Promise.resolve(mockThreadEntity));
 
+    const userIdToUsername = {
+      [`user-${idNumber1}`]: `user${idNumber1}`,
+      [`user-${idNumber2}`]: `user${idNumber2}`,
+    };
+
     mockUserRepository.getUsernameById = jest.fn()
-      .mockImplementation(() => Promise.resolve(username));
+      .mockImplementation((id) => Promise.resolve(userIdToUsername[id]));
+
+    const isDelete = false;
+
+    const commentPayloads = [
+      {
+        id: `comment-${idNumber1}`,
+        content: `Thread body ${idNumber1}`,
+        owner: credentialId,
+        date,
+      }, {
+        id: `comment-${idNumber2}`,
+        content: `Thread body ${idNumber2}`,
+        owner: `user-${idNumber2}`,
+        date,
+      },
+    ];
+
+    const mockThreadCommentEntities = [
+      new ThreadCommentEntity({
+        ...commentPayloads[0],
+        thread: threadId,
+        isDelete,
+      }),
+      new ThreadCommentEntity({
+        ...commentPayloads[1],
+        thread: threadId,
+        isDelete,
+      }),
+    ];
 
     mockThreadCommentRepository.getThreadCommentsByThreadId = jest.fn()
       .mockImplementation(() => Promise.resolve(mockThreadCommentEntities));
@@ -76,16 +77,19 @@ describe('GetThreadByIdResponseUseCase', () => {
       userRepository: mockUserRepository,
     });
 
-    const threadEntity = await getThreadByIdResponseUseCase.execute(threadId);
+    const threadResult = await getThreadByIdResponseUseCase.execute(threadId);
 
-    expect(threadEntity).toStrictEqual({
+    expect(threadResult).toStrictEqual({
       thread: {
-        id: mockThreadEntity.id,
-        title: mockThreadEntity.title,
-        body: mockThreadEntity.body,
-        date: mockThreadEntity.date,
-        username,
-        comments: expectedComments,
+        id: threadId,
+        title,
+        body,
+        date,
+        username: userIdToUsername[credentialId],
+        comments: commentPayloads.map(({ owner, ...rest }) => ({
+          ...rest,
+          username: userIdToUsername[owner],
+        })),
       },
     });
 
